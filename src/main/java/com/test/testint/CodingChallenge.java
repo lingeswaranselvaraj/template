4032ef140a616fd4f1c0e62f660540069ba2e569
linkesh <linkesheee@gmail.com>
2022-07-16 22:47:39 +0800

    second commit

---
+++ src/main/java/com/test/testint/CodingChallenge.java
@@ -0,0 +1,18 @@
+package com.test.testint;
+
+public class CodingChallenge {
+    public String sayHello() {
+        return "Hello";
+    }
+
+    public static int test (int a, int b){
+        int sum = a+b;
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        test(1,2);
+        
+    }
+}
