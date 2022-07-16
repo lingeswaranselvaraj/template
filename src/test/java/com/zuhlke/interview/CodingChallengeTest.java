5e9e7d3710d74ce8403e56b7d509a9f4c9ae745e
linkesh <linkesheee@gmail.com>
2022-07-16 22:46:32 +0800

    first commit

--- src/test/java/com/zuhlke/interview/CodingChallengeTest.java
+++ src/test/java/com/zuhlke/interview/CodingChallengeTest.java
@@ -3,9 +3,16 @@ package com.zuhlke.interview;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.test.testint.CodingChallenge;
+
 public class CodingChallengeTest {
     @Test
     public void testSayHello() {
         Assertions.assertEquals("Hello", new CodingChallenge().sayHello());
     }
+    @Test
+    public void testArithatic(){
+    Assertions.assertEquals(10, new CodingChallenge().test(1,2));
+    }
+
 }
